bd6ca2e959fa1ed19a5ea25e5e468489c70ee245
Daniel Chuks <80325389+danielChuks@users.noreply.github.com>
2023-04-03 15:54:44 +0100

    Implemented Navigation bar design

--- resources/components/shared/navigationBar/index.tsx
+++ resources/components/shared/navigationBar/index.tsx
@@ -4,6 +4,8 @@ import logo from "../../../../public/images/Group.svg";
 import SearchBar from "../searchBar";
 import Link from "next/link";
 import { IoIosNotificationsOutline } from "react-icons/Io";
+import { RiArrowDropDownFill } from "react-icons/ri";
+import face from "../../../../public/images/my-face.jpg";
 
 export default function TopNav() {
   return (
@@ -14,16 +16,24 @@ export default function TopNav() {
       <div>
         <SearchBar />
       </div>
-     
-      <div className={styles['right-nav']}>
-      <div>
-        <Link href="#">Docs</Link>
-      </div>
+
+      <div className={styles["right-nav"]}>
         <div>
-          <IoIosNotificationsOutline />
+          <Link href="#">Docs</Link>
         </div>
-        <div>
-          
+        <div >
+          <IoIosNotificationsOutline className={styles['notifications']}/>
+        </div>
+        <div className={styles["image"]}>
+          <Image src={face} height={48} width={48} alt="image" />
+        </div>
+        <div className={styles["profile"]}>
+          <div>Daniel</div>
+          <div>
+            <Link href={"#"}>
+              <RiArrowDropDownFill />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
