b43aa00e2c94c67e7c6d99ff893fa557fefebdd8
Daniel Chuks <danieluchechukwuchuks1@gmail.com>
2023-05-09 18:50:45 +0100

    fixed the navbar

--- pulchrial/src/components/shared/TopNav/index.tsx
+++ pulchrial/src/components/shared/TopNav/index.tsx
@@ -1,9 +1,9 @@
-"use client";
-import styles from "./index.module.scss";
-import { Notification } from "../notification";
-import { AiOutlineMenu } from "react-icons/ai";
-import SideBar from "../sideMenu";
-import { useState } from "react";
+'use client';
+import styles from './index.module.scss';
+import { Notification } from '../notification';
+import { AiOutlineMenu } from 'react-icons/ai';
+import SideBar from '../sideMenu';
+import { useState } from 'react';
 
 export default function TopNav() {
     const [showSidebar, setShowSidebar] = useState(true);
@@ -14,32 +14,33 @@ export default function TopNav() {
 
     return (
         <>
-            <div className={styles["container"]}>
-                <div className={styles["toggle"]} onClick={toggle}>
+            <div className={styles['container']}>
+                <div className={styles['toggle']} onClick={toggle}>
                     <AiOutlineMenu />
                 </div>
-                <div className={styles["logo"]}>pulchrial</div>
-                <div className={styles["detail-section"]}>
-                    <div className={styles["sub-section"]}>
+                <div className={styles['logo']}>pulchrial</div>
+                <div className={styles['detail-section']}>
+                    <div className={styles['sub-section']}>
                         <div>About</div>
                         <div>Contact </div>
                     </div>
                     <Notification />
-                    <div className={styles["avi"]}>
+                    <div className={styles['avi']}>
                         <img
                             src={
-                                "https://res.cloudinary.com/dhkgwi02z/image/upload/v1683640119/cld-sample.jpg"
+                                'https://res.cloudinary.com/dhkgwi02z/image/upload/v1683640119/cld-sample.jpg'
                             }
                         />
                     </div>
                 </div>
             </div>
-
-            {!showSidebar && (
-                <div className={styles['sidebar']}>
-                    <SideBar />
-                </div>
-            )}
+            <div className={styles['sideBar-wrapper']}>
+                {!showSidebar && (
+                    <div className={styles['sidebar']}>
+                        <SideBar />
+                    </div>
+                )}
+            </div>
         </>
     );
 }
