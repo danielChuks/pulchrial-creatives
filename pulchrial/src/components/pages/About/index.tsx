56347ba8c8c20e9779cadee8c59a2396446898cc
Daniel chuks <danieluchechukwuchuks1@gmail.com>
2023-05-17 14:42:17 +0100

    removed some code

--- pulchrial/src/components/pages/About/index.tsx
+++ pulchrial/src/components/pages/About/index.tsx
@@ -21,16 +21,7 @@ export default function AboutPage() {
                         timeless designs. The brand was founded with a vision to
                         create clothing that is not only beautiful but also
                         empowering and reflects the unique personality of the
-                        wearer. Our design philosophy is centered around
-                        creating clothing that blends modern and traditional
-                        styles, resulting in a unique and timeless aesthetic
-                        that stands the test of time. We draw inspiration from
-                        the beauty and diversity of the world around us, from
-                        art and culture to nature and the environment. This
-                        fusion of influences enables us to create designs that
-                        are original, bold, and distinctive. We are committed to
-                        using only the finest quality fabrics and materials in
-                        all our designs.
+                        wearer. 
                     </div>
                 </div>
                 <div className={styles["section-2"]}>
@@ -50,8 +41,8 @@ export default function AboutPage() {
                         that our clothing is not only beautiful but also
                         responsible. In addition to our
                         clothing, we also offer a range of accessories,
-                        including handbags, jewelry, and scarves. Each accessory
-                        is carefully curated to complement our clothing designs,
+                        including handbags, jewelry, and scarves.
+                        {/* Each accessory is carefully curated to complement our clothing designs,
                         adding the perfect finishing touch to any outfit. In
                         conclusion, Pulchrial Creatives is a fashion brand that
                         redefines fashion with elegance, creativity, and
@@ -60,7 +51,7 @@ export default function AboutPage() {
                         our clients to express their unique personality and
                         style. With our dedication to quality, sustainability,
                         and ethical practices, we are setting a new standard for
-                        the fashion industry.
+                        the fashion industry. */}
                     </div>
                 </div>
             </div>
