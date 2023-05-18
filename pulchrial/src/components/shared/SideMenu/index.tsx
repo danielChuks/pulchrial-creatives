197e867cf6a5f56c958cf153f5e7ea7f124c5d09
Daniel chuks <danieluchechukwuchuks1@gmail.com>
2023-05-18 17:07:48 +0100

    fixed sidebar issue

--- pulchrial/src/components/shared/SideMenu/index.tsx
+++ pulchrial/src/components/shared/SideMenu/index.tsx
@@ -7,7 +7,6 @@ import Link from 'next/link';
 export default function SideBar() {
     return (
         <div className={styles['container']}>
-
             <div className={styles['content']}>
                 <div className={styles['florish']}>
                     <div>
